54f953610f15ea18eb137b5ee52f7efd83e0b54b
tstarkv <49245152+tstarkv@users.noreply.github.com>
2019-04-30 20:11:43 +0530

    Update object_detection.py

--- research/object_detection/object_detection.py
+++ research/object_detection/object_detection.py
@@ -33,6 +33,8 @@ from PIL import Image
 # This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
 sys.path.append('./models/research/')
+print("Reached here")
+print(sys.path)
 from object_detection.utils import ops as utils_ops
 
 if StrictVersion(tf.__version__) < StrictVersion('1.12.0'):
